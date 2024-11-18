9dea3764829560b87b36ce01ab247e2fe5281130
wpage <wpage@andrew.cmu.edu>
2024-11-18 10:18:00 -0500

    setup of xcode project

---
+++ Apple Sound Sampler/Apple Sound Sampler/Apple_Sound_SamplerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Sound_SamplerApp.swift
+//  Apple Sound Sampler
+//
+//  Created by Will Page on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Sound_SamplerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple Sound Sampler/Apple Sound SamplerTests/Apple_Sound_SamplerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Sound_SamplerTests.swift
+//  Apple Sound SamplerTests
+//
+//  Created by Will Page on 11/18/24.
+//
+
+import Testing
+@testable import Apple_Sound_Sampler
+
+struct Apple_Sound_SamplerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
